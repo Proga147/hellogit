3ba9f25a22c34a80eff677ddad190264d2e774e6
Guest <proga147@users.noreply.github.com>
2017-01-26 15:18:38 -0400

    Added header file

---
+++ Cpp.h
@@ -0,0 +1,24 @@
+class Student {
+
+public:
+Student::Student( ) {
+	age=0;
+}
+
+void setname(string x ){
+name=x;
+}
+string getname( ) {
+return name;
+}
+
+void setage( int a) {
+age=a;
+}
+int getage( ) {
+	return age;}
+
+private:
+string name;
+int age;
+};
